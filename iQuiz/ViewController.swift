15bb0a8227eb0851418d4ba4894e4b96f3a724f5
Danfeng Yang <ydf1014@uw.edu>
2021-05-06 23:08:05 -0700

    writing controller

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class quizSource:NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath)
+        cell.textLabel?.text = quizTitle[indexPath.row]
+        cell.detailTextLabel?.text = quizDescription[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    let quizTitle = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let quizDescription = ["Dealing with numbers","Characters in the Marvel Universe","Systematic study of our world"]
+}
 
+
+class ViewController: UIViewController {
+    
+    let data = quizSource()
+    @IBOutlet weak var quizTable: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        quizTable.dataSource = data
+        quizTable.tableFooterView = UIView()
+        
     }
 
 
