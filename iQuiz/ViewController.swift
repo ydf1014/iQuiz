010e8d77cbe07648890458ea450cb78c7d9af185
Danfeng Yang <ydf1014@uw.edu>
2021-05-13 23:59:05 -0700

    done

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,40 +7,52 @@
 
 import UIKit
 
-class quizSource:NSObject, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return quizTitle.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath)
-        cell.textLabel?.text = quizTitle[indexPath.row]
-        cell.detailTextLabel?.text = quizDescription[indexPath.row]
-
-        return cell
-    }
-
-
-    let quizTitle = ["Mathematics", "Marvel Super Heroes", "Science"]
-    let quizDescription = ["Dealing with numbers","Characters in the Marvel Universe","Systematic study of our world"]
+class QuizCell: UITableViewCell {
+    @IBOutlet var title : UILabel?
+    @IBOutlet var description : UILabel?
+    @IBOutlet weak var img : UIImageView?
 }
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let data = quizSource()
+    
+    
+//    let data = quizSource()
     @IBOutlet weak var quizTable: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        quizTable.dataSource = data
+//        quizTable.dataSource = data
+        self.quizTable.dataSource = self
+        self.quizTable.delegate = self
+        for index in 0...(quizDescription.count - 1) {
+            quizCell.append((title: quizTitle[index], description: quizDescription[index], img: quizImage[index]))
+        }
         
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizTitle.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath) as! QuizCell
+        cell.title?.text = quizTitle[indexPath.row]
+        cell.description?.text = quizDescription[indexPath.row]
+        cell.img?.image = quizImage[indexPath.row]
+        
+        return cell
+    }
+
+
+    let quizTitle = ["Mathematics", "Marvel Super Heroes", "Science"]
+    let quizDescription = ["Dealing with numbers","Characters in the Marvel Universe","Systematic study of our world"]
+    let quizImage: [UIImage] = [UIImage(named: "math")!, UIImage(named: "marvel")!, UIImage(named: "science")!]
+    var quizCell: [(title: String, description: String, img: UIImage)] = [(title: String, description: String, img: UIImage)]()
+   
+    
     @IBAction func popSettings(_ sender: Any) {
         let alert = UIAlertController(title: "Settings", message: "OK", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Good to go"), style: .default, handler: {
